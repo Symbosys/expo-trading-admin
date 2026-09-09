cbf076bfd0602ada95fe825e77bd804e67e42041
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-09-09 15:32:29 +0530

    changed api

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://trading-omega-sepia.vercel.app/api",
+  baseURL: "https://api.expotradex.com/api",
   // baseURL: "http://localhost:4000/api",
   timeout: 8000,
   withCredentials: true,
